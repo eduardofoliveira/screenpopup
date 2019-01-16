e1ea617108325ab8140c06785b739bab8995c56a
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-01-16 14:54:12 -0200

    Add SocketPath

--- service/mysql.js
+++ service/mysql.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise')
 
 const connection = mysql.createPool({
   host: '18.228.130.32',
+  socketPath: '/var/run/mysqld/mysqld.sock',
   user: 'popup',
   password: 'B@lpha9001',
   database: 'popup',
