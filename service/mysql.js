8c291e12e2f69fa331a7e0b4d67700e388d6957e
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-05 11:21:12 -0200

    Alteração keepalive

--- service/mysql.js
+++ service/mysql.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise')
 
 const connection = mysql.createPool({
-  host: '18.228.130.32',
+  //host: '18.228.130.32',
   socketPath: '/var/run/mysqld/mysqld.sock',
   user: 'popup',
   password: 'B@lpha9001',
@@ -10,13 +10,5 @@ const connection = mysql.createPool({
   connectionLimit: 15,
   queueLimit: 0
 })
-
-setInterval(async () => {
-  await connection.query('SELECT 1 + 1')
-    .catch(error => {
-      console.log(error)
-    })
-  console.log('KeepAlive DB ' + new Date().toLocaleString())
-}, 300000)
 
 module.exports = connection
