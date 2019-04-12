ee7a4ead8aaaf9857c55fd91fd839b99209c7c3b
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-04-12 15:37:57 -0300

    Correção do subscribe do usuario

--- service/mysql.js
+++ service/mysql.js
@@ -1,22 +1,21 @@
-const mysql = require('mysql2/promise')
+const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  //host: '18.228.130.32',
-  socketPath: '/var/run/mysqld/mysqld.sock',
-  user: 'popup',
-  password: 'B@lpha9001',
-  database: 'popup',
+  host: "35.171.122.245",
+  //socketPath: '/var/run/mysqld/mysqld.sock',
+  user: "popup",
+  password: "B@lpha9001",
+  database: "popup",
   waitForConnections: true,
   connectionLimit: 15,
   queueLimit: 0
-})
+});
 
 setInterval(async () => {
-  await connection.query('SELECT 1 + 1')
-    .catch(error => {
-      console.log(error)
-    })
-  console.log('KeepAlive DB ' + new Date().toLocaleString())
-}, 120000)
+  await connection.query("SELECT 1 + 1").catch(error => {
+    console.log(error);
+  });
+  console.log("KeepAlive DB " + new Date().toLocaleString());
+}, 120000);
 
-module.exports = connection
+module.exports = connection;
