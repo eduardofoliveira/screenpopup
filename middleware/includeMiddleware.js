49d2ac329c965979926fb31c8d433625d5487aec
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-14 19:53:30 -0300

    Add Header SameSite

--- middleware/includeMiddleware.js
+++ middleware/includeMiddleware.js
@@ -1,5 +1,6 @@
 const init = (conn, io) => {
   return (middleware = (req, res, next) => {
+    res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
     req.conn = conn
     req.io = io
 
