ebcd0dcc81459d0687c6d1ab387d069880e27370
Eduardo Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2021-04-08 12:30:19 -0300

    axios rejectUnauthorized

--- service/api-zendesk.js
+++ service/api-zendesk.js
@@ -1,5 +1,5 @@
 //const querystring = require('querystring')
-const axios = require("axios");
+const axios = require("axios")
 //const fs = require('fs')
 //const FormData = require('form-data')
 
@@ -10,14 +10,15 @@ const addZenTicketCanal = async (sub_dominio, email, token, from, to, basix_user
         baseURL: `https://${sub_dominio}.zendesk.com/api/v2/`,
         auth: {
           username: `${email}/token`,
-          password: token
+          password: token,
         },
-        headers: { "Content-Type": "application/json" }
-      });
+        headers: { "Content-Type": "application/json" },
+        rejectUnauthorized: false,
+      })
 
-      let { data } = await api.get(`/users/search.json?query=${from}`);
+      let { data } = await api.get(`/users/search.json?query=${from}`)
       if (data.users.length > 0) {
-        requester_id = data.users[0].id;
+        requester_id = data.users[0].id
 
         await api.post("/tickets.json", {
           ticket: {
@@ -26,14 +27,14 @@ const addZenTicketCanal = async (sub_dominio, email, token, from, to, basix_user
               body: `Chamada Recebida:
   De: ${from}
   Para: ${to}
-  Usuário: ${basix_user}`
+  Usuário: ${basix_user}`,
             },
             priority: "urgent",
             requester_id,
             tags: ["telefonia", "Basix"],
-            brand_id
-          }
-        });
+            brand_id,
+          },
+        })
       } else {
         await api.post("/tickets.json", {
           ticket: {
@@ -42,7 +43,7 @@ const addZenTicketCanal = async (sub_dominio, email, token, from, to, basix_user
               body: `Chamada Recebida:
   De: ${from}
   Para: ${to}
-  Usuário: ${basix_user}`
+  Usuário: ${basix_user}`,
             },
             priority: "urgent",
             requester: {
@@ -50,21 +51,21 @@ const addZenTicketCanal = async (sub_dominio, email, token, from, to, basix_user
               name: from,
               phone: from,
               time_zone: "Brasilia",
-              iana_time_zone: "America/Sao_Paulo"
+              iana_time_zone: "America/Sao_Paulo",
             },
             tags: ["telefonia", "Basix"],
-            brand_id
-          }
-        });
+            brand_id,
+          },
+        })
       }
 
-      resolve();
+      resolve()
     } catch (error) {
-      console.log(error.response);
-      reject(error);
+      console.log(error.response)
+      reject(error)
     }
-  });
-};
+  })
+}
 
 const addZenTicket = (sub_dominio, email, token, from, to, basix_user) => {
   return new Promise(async (resolve, reject) => {
@@ -73,14 +74,14 @@ const addZenTicket = (sub_dominio, email, token, from, to, basix_user) => {
         baseURL: `https://${sub_dominio}.zendesk.com/api/v2/`,
         auth: {
           username: `${email}/token`,
-          password: token
+          password: token,
         },
-        headers: { "Content-Type": "application/json" }
-      });
+        headers: { "Content-Type": "application/json" },
+      })
 
-      let { data } = await api.get(`/users/search.json?query=${from}`);
+      let { data } = await api.get(`/users/search.json?query=${from}`)
       if (data.users.length > 0) {
-        requester_id = data.users[0].id;
+        requester_id = data.users[0].id
 
         await api.post("/tickets.json", {
           ticket: {
@@ -89,13 +90,13 @@ const addZenTicket = (sub_dominio, email, token, from, to, basix_user) => {
               body: `Chamada Recebida:
 De: ${from}
 Para: ${to}
-Usuário: ${basix_user}`
+Usuário: ${basix_user}`,
             },
             priority: "urgent",
             requester_id,
-            tags: ["telefonia", "Basix"]
-          }
-        });
+            tags: ["telefonia", "Basix"],
+          },
+        })
       } else {
         await api.post("/tickets.json", {
           ticket: {
@@ -104,7 +105,7 @@ Usuário: ${basix_user}`
               body: `Chamada Recebida:
 De: ${from}
 Para: ${to}
-Usuário: ${basix_user}`
+Usuário: ${basix_user}`,
             },
             priority: "urgent",
             requester: {
@@ -112,22 +113,22 @@ Usuário: ${basix_user}`
               name: from,
               phone: from,
               time_zone: "Brasilia",
-              iana_time_zone: "America/Sao_Paulo"
+              iana_time_zone: "America/Sao_Paulo",
             },
-            tags: ["telefonia", "Basix"]
-          }
-        });
+            tags: ["telefonia", "Basix"],
+          },
+        })
       }
 
-      resolve();
+      resolve()
     } catch (error) {
-      console.log(error.response);
-      reject(error);
+      console.log(error.response)
+      reject(error)
     }
-  });
-};
+  })
+}
 
 module.exports = {
   addZenTicketCanal,
-  addZenTicket
-};
+  addZenTicket,
+}
